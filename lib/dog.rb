778bb309f7abf0587fd5a1fecc885c26e56ac08e
github username <github email address>
2019-11-24 12:27:26 -0700

    completed lab and learned how to use multiple binding.pry with require 'pry' method

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,19 @@
-Please push to learn.co... not collaborators...
+require 'pry'
+
+class Dog
+    def name=(dog_name)
+        @this_dogs_name = dog_name
+    end
+
+    def name
+        @this_dogs_name
+    end
+end
+
+lassie = Dog.new
+lassie.name = "Lassie"
+binding.pry
+
+puts lassie.name
+
+binding.pry
